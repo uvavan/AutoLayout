a5308d59ba7e4295e1641d239162f2f6176d0c38
Admin <vitaliy.kazakoff2013@gmail.com>
2017-10-27 11:26:16 +0300

    Create project

---
+++ AutoLayout/SmileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Admin on 27.10.17.
+//  Copyright © 2017 Bioprom. All rights reserved.
+//
+
+import UIKit
+
+class SmileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
