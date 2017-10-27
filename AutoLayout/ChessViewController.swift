bb6b681fec626af18c9f38c80f6f6c98c5a647b7
Admin <vitaliy.kazakoff2013@gmail.com>
2017-10-27 12:10:12 +0300

    Create interface two view

---
+++ AutoLayout/ChessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChessViewController.swift
+//  AutoLayout
+//
+//  Created by Admin on 27.10.17.
+//  Copyright © 2017 Bioprom. All rights reserved.
+//
+
+import UIKit
+
+class ChessViewController: UIView {
+
+    
+
+}
--- AutoLayout/SmileViewController.swift
+++ AutoLayout/SmileViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class SmileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func backToStartScreen(_ sender: UIStoryboardSegue){
+        
     }
-
-
 }
 
