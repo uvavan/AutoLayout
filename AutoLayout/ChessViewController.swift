e43dd4bff61e3d64794aeb6ce4e9bf41d335fdcf
Admin <vitaliy.kazakoff2013@gmail.com>
2017-10-29 19:20:12 +0200

    fixed the rendering of the chess board.

--- AutoLayout/ChessViewController.swift
+++ AutoLayout/ChessViewController.swift
@@ -12,61 +12,57 @@ class ChessViewController: UIViewController {
     
     private var chessBoard: [UIView] = []
     private var stackVertical: [UIStackView] = []
-    private var stackHorizontal: UIStackView!
-    private var constraintChessBoardHorizontal: NSLayoutConstraint!
-    private var constraintChessBoardVertical: NSLayoutConstraint!
-    private var constraintLeft: NSLayoutConstraint!
-    private var constraintRight: NSLayoutConstraint!
+    @IBOutlet private weak var stackViewChess: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for parity in 0...63 {
+        var whiteOrBlack = false
+        for parity in 1...64 {
             chessBoard.append(UIView())
-            chessBoard[parity].frame.size.height = (self.view.bounds.width - 40)/8
-            chessBoard[parity].frame.size.width = (self.view.bounds.width - 40)/8
-            chessBoard[parity].backgroundColor = parity % 2 == 0 ? .white : .black
-        }
-        for index in 0...7 {
-            stackVertical.append(UIStackView())
-            stackVertical[index].frame.size.height = self.view.bounds.width - 40
-            stackVertical[index].frame.size.width = (self.view.bounds.width - 40) / 8
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        stackHorizontal = UIStackView()
-        stackHorizontal.frame.size.width = self.view.bounds.width - 40
-        stackHorizontal.frame.size.height = self.view.bounds.width - 40
-        stackHorizontal.backgroundColor = .black
-        stackHorizontal.isHidden = false
-        stackHorizontal.axis = .horizontal
-        stackHorizontal.alignment = .fill
-        stackHorizontal.distribution = .fillEqually
-        stackHorizontal.contentMode = .scaleToFill
-        stackHorizontal.isBaselineRelativeArrangement = false
-        for (count, viewChess) in chessBoard.enumerated() {
-            var index = 0
-            stackVertical[index].addSubview(viewChess)
-            if count % 8 == 0 {
-                index = index + 1
+            chessBoard[parity - 1].backgroundColor = whiteOrBlack ? .black : .white
+            if parity % 8 != 0 {
+                whiteOrBlack = !whiteOrBlack
             }
         }
-        for stackV in stackVertical {
-            stackV.isHidden = false
-            stackV.axis = .vertical
-            stackV.alignment = .fill
-            stackV.distribution = .fillEqually
-            stackV.contentMode = .scaleToFill
-            stackHorizontal.addSubview(stackV)
+        for _ in 0...7 {
+            stackVertical.append(UIStackView())
         }
-        constraintChessBoardVertical = NSLayoutConstraint(item: stackHorizontal, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY,multiplier: 1.0, constant: 0)
-        constraintChessBoardHorizontal = NSLayoutConstraint(item: stackHorizontal, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX , multiplier: 1.0, constant: 0)
-        constraintLeft = NSLayoutConstraint(item: stackHorizontal, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 20)
-        constraintRight = NSLayoutConstraint(item: stackHorizontal, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 20)
         
-        view.addSubview(stackHorizontal)
-        view.addConstraints([constraintRight, constraintLeft, constraintChessBoardVertical, constraintChessBoardHorizontal])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        for stackV in stackVertical {
+            stackV.alignment = .fill
+            stackV.axis = .vertical
+            stackV.distribution = .fillEqually
+            //stackV.spacing = 0
+           // stackV.isHidden = false
+            stackViewChess.addArrangedSubview(stackV)
+        }
+        for (index,viewChess) in chessBoard.enumerated() {
+            stackVertical[Int(index / 8)].addArrangedSubview(viewChess)
+//            switch index {
+//            case 0...7 :
+//                stackVertical[0].addArrangedSubview(viewChess)
+//            case 8...15:
+//                stackVertical[1].addArrangedSubview(viewChess)
+//            case 16...23:
+//                stackVertical[2].addArrangedSubview(viewChess)
+//            case 24...31:
+//                stackVertical[3].addArrangedSubview(viewChess)
+//            case 32...39:
+//                stackVertical[4].addArrangedSubview(viewChess)
+//            case 40...47:
+//                stackVertical[5].addArrangedSubview(viewChess)
+//            case 48...55:
+//                stackVertical[6].addArrangedSubview(viewChess)
+//            case 56...63:
+//                stackVertical[7].addArrangedSubview(viewChess)
+//            default:
+//                break
+//            }
+        }
     }
     
     
